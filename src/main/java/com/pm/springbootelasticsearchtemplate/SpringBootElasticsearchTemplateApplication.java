ffd39e31c58f6d8a7223c5392e5b440a391b65bb
Parth Makadiya <makadiyaparth98@gmail.com>
2021-02-19 06:59:57 +0530

    initial commit

---
+++ src/main/java/com/pm/springbootelasticsearchtemplate/SpringBootElasticsearchTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.pm.springbootelasticsearchtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticsearchTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElasticsearchTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pm/springbootelasticsearchtemplate/SpringBootElasticsearchTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.springbootelasticsearchtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticsearchTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
